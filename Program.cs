bc684f6ea47bc95a0751653af2423bd3ccff4d12
Natalia Kushnir <natalka7618@yandex.ru>
2022-06-14 15:33:39 +0300

    Program modified

--- Program.cs
+++ Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿string[] arraySource = new string[10] {"hi", "goodbye", "hello", "tiger", "wolf", "morning", "friend", "pork", "guy", "thanks"};
+int resLength = 0;
+
+for (int i = 0; i < arraySource.Length; i++)
+{
+    if (arraySource[i].Length <= 3)
+    {
+        resLength +=1;
+    }
+}
+
+if (resLength > 0)
+{
+    string[] res = new string[resLength];
+    int k = 0;
+    for (int i = 0; i < arraySource.Length; i++)
+    {
+        if (arraySource[i].Length <= 3)
+        {
+            res[k] = arraySource[i];
+            k++;
+        }
+    }
+    PrintArray(res);
+} 
+else
+{
+    Console.Write("Подходящих элементов нет");
+}
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i != arr.Length - 1)
+        {
+            Console.Write(arr[i] + ", ");
+        }
+        else
+        {
+            Console.Write(arr[i]);
+        }
+    }
+    Console.Write("]");
+}
